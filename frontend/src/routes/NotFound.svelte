206ace118fa9a05b7947c26ce474d394d520ec3a
ITGuyCzechia <92447254+ITGuyCzechia@users.noreply.github.com>
2023-11-17 18:42:02 +0100

    Update NotFound.svelte
    Překlad do češtiny

--- frontend/src/routes/NotFound.svelte
+++ frontend/src/routes/NotFound.svelte
@@ -3,11 +3,11 @@
 	<div class="container flex flex-col items-center justify-center px-5 mx-auto my-8">
 		<div class="max-w-md text-center">
 			<h2 class="mb-8 font-extrabold text-9xl dark:text-gray-600">
-				<span class="sr-only">Error</span>404
+				<span class="sr-only">Chyba</span>404
 			</h2>
-			<p class="text-2xl font-semibold md:text-3xl">Sorry, we couldn't find this page.</p>
-			<p class="mt-4 mb-8 dark:text-gray-400">But dont worry, you can find plenty of other things on our homepage.</p>
-			<a rel="noopener noreferrer" href="#" class="px-8 py-3 font-semibold rounded dark:bg-violet-400 dark:text-gray-900">Back to homepage</a>
+			<p class="text-2xl font-semibold md:text-3xl">Promiňte, ale nemohli jsme najít požadovanou stránku.</p>
+			<p class="mt-4 mb-8 dark:text-gray-400">Ale nebojte, můžete najít spoustu jiných věci na naší domovské stránce.</p>
+			<a rel="noopener noreferrer" href="#" class="px-8 py-3 font-semibold rounded dark:bg-violet-400 dark:text-gray-900">Zpět na domovskou stránku</a>
 		</div>
 	</div>
 </section>
