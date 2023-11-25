135b99f66f95bca4c92ab76b4b4985b2e187577f
Vladimir284 <vladis@toonect.com>
2023-11-25 19:23:33 +0100

    Added login modal to home

--- frontend/src/routes/Home.svelte
+++ frontend/src/routes/Home.svelte
@@ -2,10 +2,25 @@
 	import Navbar from "../components/Navbar.svelte";
 	import Button from "../components/Button.svelte";
 	import Hero from "../components/Hero.svelte";
+	import LoginModal from "../components/LoginModal.svelte";
 
-	function redirectCampRegister(){
-		location.href="#/register";
+	let modalName = "login_modal";
+	let whoClicked = "";
+
+
+	function redirectCampRegister() {
+		location.href = "#/register";
 	}
+
+	function handleClick(whoUsed) {
+		// Ignore errors with passing type since we pass to us known types
+		whoClicked = whoUsed;
+		document.getElementById(modalName).show();
+	}
+
+	const loginHandler = (e) => {
+		console.log(e.details);
+	};
 
 	// TODO Vlado
 	// Tu bude potrebne implementovat funkciu na obsluhu prihlasenia sa
@@ -13,36 +28,45 @@
 
 
 <Navbar>
-		<div slot="homePageContainer" class="flex-1">
-<!--			<Button buttonClass="btn btn-ghost text-xl" >Domovská stránka</Button>-->
-		</div>
-		<div slot="centerContainer" class="flex-none">
-			<ul class="menu menu-horizontal px-1">
-				<li><a href="#/register">Registrace tábora</a></li>
-				<li>
-					<details>
-						<summary>
-							Přihlasit se jako
-						</summary>
-						<ul class="p-2 bg-base-100">
-							<!-- TODO Implement Vlado-->
-							<!-- Tuna treba Osetrtit Vito ze pred presmerovanim
-							sa musia autentifikovat, teda musia sa prihlasit. A
-							prihlasia sa v modalnom okne -->
-							<li><a href="#/organizer">Organizátor tábora</a></li>
-							<li><a href="#/supervisor">Vedoucí</a></li>
-							<li><a href="#/attendee">Účastník</a></li>
-						</ul>
-					</details>
-				</li>
-			</ul>
-		</div>
+	<div slot="homePageContainer" class="flex-1">
+	</div>
+	<div slot="centerContainer" class="flex-none">
+		<ul class="menu menu-horizontal px-1">
+			<li><a href="#/register">Registrace tábora</a></li>
+			<li>
+				<details>
+					<summary>
+						Přihlasit se jako
+					</summary>
+					<ul class="p-2 bg-base-100">
+						<li>
+							<Button buttonClass="btn btn-ghost" on:click={() => handleClick("organizer")}>
+								Organizátor tábora
+							</Button>
+						</li>
+						<li>
+							<Button buttonClass="btn btn-ghost" on:click={() => handleClick("supervisor")}>
+								Vedoucí
+							</Button>
+						</li>
+						<li>
+							<Button buttonClass="btn btn-ghost" on:click={() => handleClick("attendee")}>
+								Účastník
+							</Button>
+						</li>
+					</ul>
+				</details>
+			</li>
+		</ul>
+	</div>
 </Navbar>
 
 <Hero>
-		<div class="max-w-md">
-			<h1 class="text-5xl font-bold">Vítejte!</h1>
-			<p class="py-6">Pro lepší zážitek nechte zmodernizovat tábor naším informačním systémem bodovaných táborových aktivit.</p>
-			<Button buttonClass="btn btn-primary" on:click={redirectCampRegister}>Začíname</Button> <!-- Na kliknutie Vito treba presmerovat na Camp Register nech si prihlasia camp-->
-		</div>
+	<LoginModal modalId={modalName} on:login={loginHandler} />
+	<div class="max-w-md">
+		<h1 class="text-5xl font-bold">Vítejte!</h1>
+		<p class="py-6">Pro lepší zážitek nechte zmodernizovat tábor naším informačním systémem bodovaných táborových
+			aktivit.</p>
+		<Button buttonClass="btn btn-primary" on:click={redirectCampRegister}>Začíname</Button>
+	</div>
 </Hero>
