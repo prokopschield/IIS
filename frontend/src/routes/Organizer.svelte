55f036a24613b9164e5ee3e3c2563786cf0368a1
ITGuyCzechia <92447254+ITGuyCzechia@users.noreply.github.com>
2023-11-26 08:00:34 +0100

    Příprava Organizer.svelte
    Vytvoření šablonu

--- frontend/src/routes/Organizer.svelte
+++ frontend/src/routes/Organizer.svelte
@@ -1,3 +1,13 @@
-<h1>
-	Stránka organizátora tábora TBD
-</h1>
+<script>
+	import Button from "../components/Button.svelte";
+	import Navbar from "../components/Navbar.svelte";
+</script>
+
+<Navbar>
+	<div slot="homePageContainer" class="flex-1">
+		<Button buttonClass="btn btn-ghost text-xl" on:click={homePageRedirect}>Domovská stránka</Button>
+	</div>
+	<div slot="centerContainer" class="flex-none">
+		
+	</div>
+</Navbar>
