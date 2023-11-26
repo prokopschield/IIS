3066ef6903e9778b540a60b88ca0ab35f1f6c7d1
Vladimir284 <vladis@toonect.com>
2023-11-26 13:44:31 +0100

    Added skelet for attendee, TBD:Implent

--- frontend/src/routes/Attendee.svelte
+++ frontend/src/routes/Attendee.svelte
@@ -1,3 +1,36 @@
-<h1>
-	Stránka  účastníka TBD
-</h1>
+<script>
+	import Navbar from "../components/Navbar.svelte";
+	import Carrousel from "../components/Carrousel.svelte";
+	import Hero from "../components/Hero.svelte";
+	import Button from "../components/Button.svelte";
+	import Card from "../components/Card.svelte";
+
+	function homePageRedirect() {
+		location.href = "#/";
+	}
+</script>
+
+
+<Navbar>
+	<div slot="homePageContainer" class="flex-1">
+		<Button buttonClass="btn btn-ghost text-xl" on:click={homePageRedirect}>Domovská stránka</Button>
+	</div>
+	<div slot="centerContainer" class="flex-none">
+	</div>
+</Navbar>
+
+<Hero>
+
+	<!-- Carrousel for upcoming activities-->
+	<Carrousel>
+		<Card></Card>
+	</Carrousel>
+
+	<!-- -->
+	<!-- Last added activities -->
+
+	<!-- Ammount of all points -->
+
+	<!-- Add sidebar menu-->
+</Hero>
+
