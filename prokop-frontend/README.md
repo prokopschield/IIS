e3603bde8dd40c17816a220f5405e2286729095d
Prokop <prokop.schield@protonmail.com>
2023-11-27 15:59:49 +0100

    feat(prokop-frontend): initial commit

---
+++ prokop-frontend/README.md
@@ -0,0 +1,3 @@
+# Alternativní frontend pro případ, že kolegové nestihnou ten, který připravují.
+
+Omlouvám se za grafickou úpravu, vznikl rychle a ve stresu.
---
+++ prokop-frontend/src/App.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import { loading } from "./lib/state";
+</script>
+
+<main>
+	{#if $loading}
+		Moment, prosím, načítám data...
+	{:else}
+		Načteno
+	{/if}
+</main>
